b9257601fc920c552736482531d33a681486cf02
rishikesh-revandikar <rishikeshrevandikar3110@gmail.com>
2024-09-26 22:23:29 +0530

    test for book

---
+++ src/tests/book.test.ts
@@ -0,0 +1,19 @@
+import { Book } from "../src/book";
+
+describe("Book Class", () => {
+  it("should create a book with valid attributes", () => {
+    const book = new Book(
+      "9780132350884",
+      "Clean Code",
+      "Robert Cecil Martin",
+      2012
+    );
+    expect(book.getISBN()).toBe("9780132350884");
+  });
+
+  it("should throw an error if required fields are missing", () => {
+    expect(
+      () => new Book("", "Clean Code", "Robert Cecil Martin", 2012)
+    ).toThrowError("All fields are required.");
+  });
+});
