74af6358d200bc788dbfde7074868c6546cf8283
rishikesh-revandikar <rishikeshrevandikar3110@gmail.com>
2024-09-26 23:03:20 +0530

    test for returning  book

--- src/tests/library.test.ts
+++ src/tests/library.test.ts
@@ -53,4 +53,42 @@ describe("Library Class", () => {
       "Book not found."
     );
   });
+
+   it("should allow a user to return a book they have borrowed", () => {
+     library.addUser(librarian);
+     library.addBook(librarian, book);
+
+     const user = new User("user1", Role.USER);
+     library.addUser(user);
+     library.borrowBook(user, book.getISBN());
+
+     library.returnBook(user, book.getISBN());
+
+     expect(library.viewAvailableBooks().size).toBe(1);
+   });
+
+   it("should throw an error when a user tries to return a book that was not borrowed", () => {
+     const user = new User("user1", Role.USER);
+     library.addUser(user);
+     library.addBook(librarian, book);
+
+     expect(() => library.returnBook(user, book.getISBN())).toThrowError(
+       "Book was not borrowed by any user"
+     );
+   });
+
+   it("should throw an error when a user tries to return a book they did not borrow", () => {
+     const user1 = new User("user1", Role.USER);
+     const user2 = new User("user2", Role.USER);
+     library.addUser(librarian);
+     library.addUser(user1);
+     library.addUser(user2);
+     library.addBook(librarian, book);
+
+     library.borrowBook(user1, book.getISBN());
+
+     expect(() => library.returnBook(user2, book.getISBN())).toThrowError(
+       "Book was not borrowed by this user"
+     );
+   });
 });
